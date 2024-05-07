4616e9e00434bce3a6c64044be230c85dc348eb6
emmaanord <128476191+emmaanord@users.noreply.github.com>
2024-05-07 16:37:07 +0200

    Kommenterat koden

--- tests/new.test.js
+++ tests/new.test.js
@@ -1,9 +1,13 @@
 // Test för att testa lägga till ett element till stacken.
+// IMporterar stack-modulen
 const stack = require('../src/stack');
 
+// Enhetstest
 test('Pushing an element adds it to the stack', () => {
+    // Definierar elementet som ska läggas till.
     const elementToAdd = "Banana";
 
+    // Använder .push för att lägga till elementet i stacken.
     stack.push(elementToAdd);
 
     expect(stack.peek()).toBe(elementToAdd);
